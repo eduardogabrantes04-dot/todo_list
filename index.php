7f0069b1668bd8779d1a6f686ac7b71193eb3150
Andressa <asalecrim7@gmail.com>
2025-10-29 10:30:52 -0300

    Criação de estilos da página inicial

--- index.php
+++ index.php
@@ -1,15 +1,41 @@
+<?php include ('db_connection.php'); ?>
+
 <!DOCTYPE html>
-<html lang = "pt-br">
+<html lang="pt-br">
     <head>
-        <meta charset = "UTF-8">
-        <title> to_do_list </title>
-        <link rel = "stylesheet" href = "style.css">
+        <meta charset="UTF-8">
+        <title>To_do_List</title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <h1> Minha To_Do_List </h1>
-        <form action = "add_task.php" method = "POST">
-        <input type = "text" name = "title" placeholder = "nova tarefa..." required>
-        <button type = "submit"> Adicionar </button>
-        </form>
-    </body>
+        <h1>
+            Minha To_do_List 
+        </h1>
+    <form action="add_task.php" method="POST">
+        <input type="text" name="title" placeholder="nova tarefa..." required>
+        <button type="submite"> Adicionar </button>
+    </form>
+    <hr>
+    <ul>
+        <?php
+        $sql = "SELECT * FROM tasks ORDER BY id DESC";
+        $result = $conn->query(query: $sql);
+        if ($result->num_rows > 0){
+            while ($row = $result->fetch_assoc()){
+            echo "<lie>";
+            echo $row ['status'] == 'cncluida' ? "<s> {$row ['tittle']} </s>": $row ['tittle'];
+            echo"
+            <a href = 'update_task.php?id={$row['id']}'>V</a>
+            <a href = 'delete_task.php?id={$row['id']}'>L</a>";
+            echo "</li>";
+
+        }
+    }else{
+        echo "<p> Nenhuma tarefa cadastrada.</php>";
+
+    }
+
+    ?>
+    </ul>
+</body>
 </html>
