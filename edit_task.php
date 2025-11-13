ad8982110a0a385356391be481417d5d024c065a
Andressa <asalecrim7@gmail.com>
2025-11-13 12:48:00 -0300

    Criação da função de editar tarefas

---
+++ edit_task.php
@@ -0,0 +1,34 @@
+<?php
+    include('db_connection.php');
+    if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM tasks WHERE id = $id";
+    $result = $conn -> query ($sql);
+    if ($result->num_rows == 1 ){
+    $task= $result -> fetch_assoc();
+    }else{
+    echo "Tarefas não informado.";
+    exit;
+    }}else{
+    echo "ID da tarefa não informada.";
+    exit;
+    }
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Editar tarefa</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1> Editar Tarefa </h1>
+        <form action="update_task_name.php" method="POST">
+            <input type = "hidden" name="id" value="<?php echo $task['id']; ?>">
+            <input type="text" name="title" value="<?php echo htmlspecialchars($task['title']); ?>"required>
+            <button type = "submit">salvar alterações </button>
+        </form>
+        <br>
+        <a href="index.php"> <-voltar </a>
+    </body>
+</html>
